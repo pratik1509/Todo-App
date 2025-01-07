f94d064ac021706fb90bf7385938649f9ccd3362
pratik1509 <gajjarpratikn@gmail.com>
2025-01-07 14:05:52 +0530

    Initial Commit

--- README.md
+++
@@ -1,7 +0,0 @@
-# EShopMicroservices
-
-This micro service is using .Net 8 minimal api, docker, docker compose and microservice architecture DDD, CQRS, Veritical/Clean Architecture.
-
-![microservices](https://github.com/aspnetrun/run-aspnetcore-microservices/assets/1147445/efe5e688-67f2-4ddd-af37-d9d3658aede4)
-
-There is a couple of microservices which implemented **e-commerce** modules over **Catalog, Basket, Discount** and **Ordering** microservices with **NoSQL (DocumentDb, Redis)** and **Relational databases (PostgreSQL, Sql Server)** with communicating over **RabbitMQ Event Driven Communication** and using **Yarp API Gateway**.
--- ToDoList/obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+001221359a9f9449529a8c6ce69310566cc06364")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
